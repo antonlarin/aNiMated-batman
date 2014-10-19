897fb1c202fe14c40e775eb5cbcfe79f182ce1d1
ruslanisrafilov <ruslan.is@outlook.com>
2014-10-20 01:25:28 +0400

    install and installnw commands added to configuration script.

--- TeamProjects/HESProject/configure.py
+++ TeamProjects/HESProject/configure.py
@@ -1,18 +1,38 @@
 import os
 import sys
 import shutil
+import urllib
+import zipfile
+import tarfile
+import platform
 import subprocess
 
+def isWindows():
+    return sys.platform.startswith("win32")
+def isLinux():
+    return sys.platform.startswith("linux")
+def isMacOS():
+    return sys.platform.startswith("darwin")
+
+def getArchitectureName():
+    arcMap = { 
+        "AMD64": "x64", "x86_64": "x64", "i686": "x64", 
+        "i386": "ia32", "x86": "ia32" 
+    }
+    return arcMap[platform.machine()]
+
+
+shellMode = isWindows()
 scriptPath = os.path.dirname(os.path.realpath(sys.argv[0]))
 def fullPath(rPath):
     return os.path.join(scriptPath, rPath)
 
-
-buildDir  = fullPath("./UI/node_modules/solver")
-solverDst = fullPath("./UI/node_modules/solver/Solver")
-solverSrc = fullPath("./Solver")
-shellMode = sys.platform.startswith("win32")
 nodeWVers = "0.10.5"
+uiDir     = fullPath("UI")
+solverSrc = fullPath("Solver")
+buildDir  = fullPath("UI/node_modules/solver")
+solverDst = fullPath("UI/node_modules/solver/Solver")
+nwUrlTmpl = "http://dl.node-webkit.org/v%s/node-webkit-v%s-%s-%s.%s"
 
 
 def moveToBuildDir():
@@ -40,6 +60,52 @@ def gypClean():
     moveToBuildDir()
     subprocess.call(["nw-gyp", "clean"], shell=shellMode)
 
+def getPlatformParams():
+    if isLinux():
+        return ("linux", getArchitectureName(), "tar.gz")
+    if isMacOS():
+        return ("osx", getArchitectureName(), "zip")
+    if isWindows():
+        return ("win", "ia32", "zip")
+    raise(Exception("We are not support current platform."))
+
+def getnwBuildUrl():
+    urlFormatArgs = [nodeWVers, nodeWVers]
+    urlFormatArgs += getPlatformParams()
+    nwUrl = nwUrlTmpl % tuple(urlFormatArgs)
+    return nwUrl
+
+def copyAllFromDir(src, dst):
+    srcFiles = os.listdir(src)
+    for fileName in srcFiles:
+        srcFileName = os.path.join(src, fileName)
+        dstFileName = os.path.join(dst, fileName)
+        print("->" + fileName)
+        if os.path.isfile(srcFileName):
+            shutil.copy(srcFileName, dstFileName)
+        else:
+            if os.path.exists(dstFileName):
+                shutil.rmtree(dstFileName)
+            shutil.copytree(srcFileName, dstFileName)
+
+def getFileNameNoExt(fileName):
+    res = os.path.splitext(fileName)[0]
+    if isLinux():
+        res = os.path.splitext(res)[0]
+    return res
+
+def unpackArchive(fileName):
+    if isLinux():
+        archive = tarfile.open(fileName)
+    else:
+        archive = zipfile.ZipFile(fileName)
+    archive.extractall(uiDir)
+    archive.close()
+    archiveRootDir = getFileNameNoExt(fileName)
+    copyAllFromDir(archiveRootDir, uiDir)
+    shutil.rmtree(archiveRootDir)
+
+
 def clearCommand():
     gypClean()
     removeSolverDir()
@@ -53,11 +119,28 @@ def rebuildCommand():
     clearCommand()
     buildCommand()
 
+def installnwCommand():
+    nwUrl = getnwBuildUrl()
+    nwArchiveName = nwUrl.split('/')[-1]
+    nwArchivePath = os.path.join(uiDir, nwArchiveName)
+    if not os.path.exists(nwArchivePath):
+        print("Downloading node-webkit..")
+        urllib.urlretrieve(nwUrl, nwArchivePath)
+    
+    print("Unpacking node-webkit..")
+    unpackArchive(nwArchivePath)
+
+def installCommand():
+    installnwCommand()
+    rebuildCommand()
+
 def executeCommand(cmd):
     cmdMap = {
         "build": buildCommand,
         "clear": clearCommand,
-        "rebuild": rebuildCommand
+        "rebuild": rebuildCommand,
+        "installnw": installnwCommand,
+        "install": installCommand
     }
     if not cmd in cmdMap:
         print("We are can't execute '%s' command." % cmd)
@@ -67,7 +150,6 @@ def executeCommand(cmd):
 
 
 def main():
-    print __file__
     if len(sys.argv) == 1:
         buildCommand()
     else:
