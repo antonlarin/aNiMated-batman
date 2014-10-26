7592bd2c1d4ce6d940fa3769b8e09e40b251190a
Anton Larin <edding.default@gmail.com>
2014-10-26 18:18:12 +0300

    Add jqPlot sources, add solver initialization and invocation to viewmodel.

--- TeamProjects/HESProject/UI/application/js/hesviewmodel.js
+++ TeamProjects/HESProject/UI/application/js/hesviewmodel.js
@@ -1,7 +1,10 @@
-var solverStorage = require("solver");
+var solverProvider = require("solver");
 
 function hesViewModel() {
   var self = this;
+
+  self.solversList = solverProvider.getSolverDescriptions();
+  self.currentSolver = solverProvider.provide(self.solversList[0]);
 
   self.rhsFunctionExpr = ko.observable("0");
   self.icFunctionExpr = ko.observable("99*x+1");
@@ -11,8 +14,23 @@ function hesViewModel() {
   self.availableSolvers = ko.observableArray([ "Explicit", "Implicit" ]);
 
   self.solve = function() {
-    console.log("Solve!")
+    self.currentSolver.setTimeLimit(parseFloat(self.timeLimit()));
+    self.currentSolver.setInitialConditions(self.icFunctionExpr());
+    self.currentSolver.leftBoundaryCondition(self.lbcFunctionExpr());
+    self.currentSolver.rightBoundaryCondition(self.rbcFunctionExpr());
+    self.currentSolver.setRHSFunction(self.rhsFunctionExpr());
+    self.currentSolver.solve();
+    console.log("Solved!");
+    $.jqplot("output",  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
   };
+
+  self.showingTable = false;
+  self.goToTable = function() {
+    self.showingTable = true;
+  }
+  self.goToLayers = function() {
+    self.showingTable = false;
+  }
 };
 
 $(document).ready(function() {
